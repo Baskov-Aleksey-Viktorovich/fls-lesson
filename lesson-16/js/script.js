f0e5462987478a6311395fee4018986b75dad29c
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2024-05-27 11:08:15 +0300

    add some js for lesson 16

--- lesson-16/js/script.js
+++ lesson-16/js/script.js
@@ -1,6 +1,13 @@
 let getBurger = document.querySelector(".menu-icon");
 let getMenu   = document.querySelector(".header__burger");
+let getSearchBtn = document.querySelector(".body-header__action-search");
+let getSearch    = document.querySelector(".search-body-header");
+
 
 getBurger.addEventListener('click', ()=> {
 	getMenu.classList.toggle("active");
 });
+
+getSearchBtn.addEventListener('click', ()=> {
+	getSearch.classList.toggle("open")
+})
