9adf380d2b73603c8d5e25218cb90df3ae437d5a
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2024-06-08 04:56:18 +0300

    make adaptive header and some js for adaptive

---
+++ task-18/js/burger.js
@@ -0,0 +1,22 @@
+let getBurger = document.querySelector(".menu-icon");
+let getMenu   = document.querySelector(".header__menu");
+
+function checkMenuWidth() {
+    if(getMenu){
+        const menuWidth = getMenu.offsetWidth;
+        if(menuWidth >= 1010 && getMenu.classList.contains('active')){
+            getMenu.toggle('active');
+        }
+    }
+    }
+
+
+
+getBurger.addEventListener('click', ()=> {
+	getMenu.classList.toggle("active");
+});
+    // Виклик функції при завантаженні сторінки
+    window.addEventListener('load', checkMenuWidth);
+
+    // Виклик функції при зміні розміру вікна
+    window.addEventListener('resize', checkMenuWidth);
