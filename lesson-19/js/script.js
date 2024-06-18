72b675df1725efb9386cef515e6d07b3aaa14122
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2024-06-19 01:28:12 +0300

    add some css and make html constractin with semantic

--- lesson-19/js/script.js
+++ lesson-19/js/script.js
@@ -1,1 +1,10 @@
-console.log('IM AM A LIVE')
+const searchIcon  = document.querySelector(".header__img");
+const searchInput = document.querySelector(".header__input");
+
+function showSearch (){
+    searchIcon.addEventListener('click', ()=>{
+        searchInput.classList.toggle('show');
+    })
+}
+
+showSearch();
