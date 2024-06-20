fe0eb04b90daa4754a59c6abbb7e561bcddbaa21
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2024-06-21 00:49:24 +0300

    add footer and try make grid category block

--- lesson-19/js/script.js
+++ lesson-19/js/script.js
@@ -1,10 +1,19 @@
 const searchIcon  = document.querySelector(".header__img");
 const searchInput = document.querySelector(".header__input");
+const upBtn       = document.querySelector('.sub__link')
 
 function showSearch (){
     searchIcon.addEventListener('click', ()=>{
         searchInput.classList.toggle('show');
     })
 }
+
+upBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
 
 showSearch();
