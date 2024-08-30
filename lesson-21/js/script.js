c76738fe2d28b015c9a77e9c2482056dacb19578
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2024-08-31 00:43:00 +0300

    output to console first OBJECT

--- lesson-21/js/script.js
+++ lesson-21/js/script.js
@@ -1,3 +1,13 @@
-console.log('js just for test');
+'use strict'; //
 
+let userAge = 26;
 
+let userData = {
+    name: 'Oleksii',
+    age: 26,
+    gender: 'Man',
+    favoriteFilm: 'Harry Potter',
+    favoriteColor: "Grey",
+}
+
+console.log(userAge, userData)
