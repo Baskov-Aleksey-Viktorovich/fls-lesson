d14ced53cc22998dabad542b20b3783e1f0b618b
Baskov-Aleksey-Viktorovich <777baskovaleksey777@gmail.com>
2024-11-27 21:51:07 +0200

    add new task home work

--- lesson-23/js/home-work.js
+++ lesson-23/js/home-work.js
@@ -11,7 +11,8 @@ let ul = document.createElement('ul');
 function addList (getQuntity){
         for(let i = 1; i <= getQuntity; i++){
             let li = document.createElement('li');
-            li.textContent = `li № ${i}`;
+            li.classList.add('item')
+/*             li.textContent = `li № ${i}`; */
             ul.appendChild(li);
         }
         getBody.appendChild(ul);
@@ -24,10 +25,18 @@ makeDiv.textContent = 'This is make div with js';
 makeDiv.classList.add('loaded');
 getBody.appendChild(makeDiv);
 
+// task 04
+
+const getItem = document.getElementsByClassName("item");
+
+for(let i = 0; i < getItem.length; i++){
+    getItem[i].classList.add('active');
+    getItem[i].textContent = (`element №${i+1}`);
+    if(getItem[i].closest('.active')){
+        getItem[i].style.color = 'red';
+    } 
+}
+
 if(makeDiv.closest('.loaded')){
     makeDiv.style.color = 'green';
 }
-
-/* if(){
-    makeDiv.style.color = 'green';
-} */
